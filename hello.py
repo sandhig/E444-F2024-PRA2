2c41a6634e68041ff2748f6fe85f6b9a0c01a7a7
sandhig <sandhi.ganjoo012@gmail.com>
2024-09-25 22:27:32 -0400

    finished example 3-11 3-12 3-13 3-14 adding local time to homepage

--- hello.py
+++ hello.py
@@ -1,14 +1,16 @@
+from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 # error handling
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
 
 @app.errorhandler(500)
 def internal_server_error(e):
@@ -17,7 +19,8 @@ def internal_server_error(e):
 # routes
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
